2ae67646d017e3508f42d6d6711b82bdc92f8e79
Jabob <jakob949@hotmail.com>
2024-10-22 09:26:38 +0200

    Introducing UnionFind for fast clustering

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from cd_hit import run_cd_hit
 from file_parsing import parse_single_file, file_exists_check, write_proteins_to_fasta
 from prot_T5 import calculate_embeddings, combine_embedding_files, concatenate_embeddings
 from pca_reduction import reduce_embeddings_with_pca
-from clustering import cluster_hdscan
+# from clustering import cluster_hdscan
 import torch
 import numpy as np
 import time
-from lsh_cluster import cluster_lsh
-from cluster_faiss import hierarchical_cluster_faiss
+from cluster_faiss import hierarchical_cluster_faiss_parallel
 
 s1 = time.time()
+times = []
 
 def main(input_dir, output_dir, max_seq_length, target_batch_size, use_bf16, pca_components, break_point=np.inf):
     os.makedirs(output_dir, exist_ok=True)
@@ -62,7 +62,8 @@ def main(input_dir, output_dir, max_seq_length, target_batch_size, use_bf16, pca
             else:
                 print(f"CD-HIT clustering failed for {file_name}")
     print("CD-HIT clustering completed.")
-
+    times.append(["CD-hit", round(s1-time.time(), 2)])
+    s2 = time.time()
     print("Creating embeddings for clustered proteins...")
     clustered_files = glob.glob(os.path.join(output_dir, "*_clustered_proteins.fasta"))
     embedding_files = []
@@ -81,41 +82,31 @@ def main(input_dir, output_dir, max_seq_length, target_batch_size, use_bf16, pca
         
         embedding_files.append(output_file)
     print("\nEmbeddings creation completed.")
-
+    times.append(["Embeddings", round(s2-time.time(), 2)])
     print("Concatenating all embeddings...")
     all_embeddings_array, all_protein_ids = concatenate_embeddings(embedding_files)
 
     print(f"Total number of proteins: {len(all_protein_ids)}")
     print(f"Shape of concatenated embeddings: {all_embeddings_array.shape}")
-    # save concatenated embeddings
+    # # save concatenated embeddings
     all_embeddings_file = os.path.join(output_dir, "all_embeddings.npz")
     np.savez(all_embeddings_file, embeddings=all_embeddings_array, protein_ids=all_protein_ids)
     # save protein ids
     all_protein_ids_file = os.path.join(output_dir, "all_protein_ids.txt")
     with open(all_protein_ids_file, "w") as f:
         f.write("\n".join(all_protein_ids))
-
-    # print(f"Performing PCA reduction to {pca_components} components...")
-    # reduced_embeddings = reduce_embeddings_with_pca(all_embeddings_array, n_components=pca_components)
-
-    # # Save the reduced embeddings
-    # reduced_file = os.path.join(output_dir, f"all_reduced_embeddings_pca{pca_components}.npz")
-    # np.savez(reduced_file, embeddings=reduced_embeddings, protein_ids=all_protein_ids)
-    # print(f"Reduced embeddings saved to: {reduced_file}")
     s_c = time.time()
-    print("Clustering reduced embeddings...")
-    # cluster_df = cluster_lsh(reduced_file, threshold=0.9, hash_size=6, num_hashtables=5)
-    # cluster_df.to_csv(os.path.join(output_dir, "clustered_proteins_lsh.csv"), index=False)
-    # cluster_df = cluster_hdscan(reduced_file)
-    # cluster_df.to_csv(os.path.join(output_dir, "clustered_proteins.csv"), index=False)
-
+    
     print("Clustering reduced embeddings...")
     cluster_df = hierarchical_cluster_faiss(all_embeddings_file, 
                                         similarity_threshold=0.95, 
                                         core_threshold=0.95, 
                                         shell_threshold=0.15, 
                                         gpu=True,
-                                        n_neighbors=10)    
+                                        n_neighbors=15)    
+
+    
+    times.append(["Clustering", round(s_c-time.time(), 2)])
     cluster_df.to_csv(os.path.join(output_dir, "clustered_proteins.csv"), index=False)
     print(f"Clustering completed. Time taken: {round((time.time()-s_c)/60,2)} minutes")
 
@@ -123,21 +114,27 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run pan-genome analysis, create protein embeddings, and reduce dimensionality")
     parser.add_argument("--input_dir", 
                         help="Glob pattern for input genome files (GFF3, GenBank, FASTA)",
-                        default="/data/nilar/pan_genome/genomes/selection/*.gff3")
+                        default="/data/nilar/pan_genome/genomes/*.gff3")
     parser.add_argument("--output_dir", 
                         help="Directory to save output files",
-                        default="/data/nilar/pan_genome/full_analysis_output/9_genes_large")
+                        default="/data/nilar/pan_genome/full_analysis_output/bp_20_all_peps_seq_large")
     parser.add_argument("--max_seq_length", type=int, default=12500, 
                         help="Maximum sequence length for protein embeddings")
     parser.add_argument("--target_batch_size", type=int, default=1, 
                         help="Target batch size for embedding calculations")
-    parser.add_argument("--use_bf16", action="store_false", default=True, 
+    parser.add_argument("--use_bf16", action="store_true", default=True, 
                         help="Use bfloat16 precision for embeddings (default: True)")
     parser.add_argument("--pca_components", type=int, default=450,
                         help="Number of components to keep after PCA reduction")
+    parser.add_argument("--break_point", type=int, default=np.inf,
+                        help="Number of files to process before stopping")
     args = parser.parse_args()
 
-    main(args.input_dir, args.output_dir, args.max_seq_length, args.target_batch_size, args.use_bf16, args.pca_components)
+    main(args.input_dir, args.output_dir, args.max_seq_length, args.target_batch_size, args.use_bf16, args.pca_components, args.break_point)
 
 s2 = time.time()
 print(f"Total time: {round((s2-s1)/60,2)} minutes")
+with open(os.path.join(args.output_dir, "times.txt"), "w") as f:
+    for name, seconds in times:
+        f.write(f"{name}: {seconds} seconds\n")
+    f.write(f"Total time: {round((s2-s1),2)} seconds\n")
